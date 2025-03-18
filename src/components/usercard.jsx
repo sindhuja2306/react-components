10e54fa8a874bf5f8b952e079d220de43f62a27b
paila sindhuja <sindhujapaila102@gmail.com>
2025-03-18 11:15:06 +0530

    add

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,18 @@
 // write the component code here
+import React from "react";
+
+function Card(){
+    return(
+        <div>
+            <img src="https://img.freepik.com/premium-vector/avatar-profile-icon-flat-style-female-user-profile-vector-illustration-isolated-background-women-profile-sign-business-concept_157943-38866.jpg?semt=ais_hybrid" width="200px"></img>
+            <h2>Sindhuja</h2>
+            <p>sindhujapaila102@gmail.com</p>
+            <p>9999999999</p>
+            <p>Andhra Pradesh</p>
+        </div>
+
+    );
+}
+
+
+export default Card;
